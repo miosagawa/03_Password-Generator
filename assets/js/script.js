aedd9b28bdbb755d75dfe96284c35fd40aa1389f
Mio Sagawa <miosagawa721@gmail.com>
2020-08-22 00:43:04 -0500

    submit10

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
+var lowercase = ["a", "b", "c"];
+var uppercase = ["A", "B", "C"];
+var number = ["1", "2", "3"];
+var special = ["!", "@", "#"];
+
+function generatePassword(){
+  var length = prompt("How many character would you like you password to be?");
+  if(length < 8){
+    alert("Password must be longer than 8");
+    return;
+  }
+  if(length > 128){
+    alert("Password must be less than 128");
+    return;
+  }
+
+  var hasLowercase = confirm("Click Ok for lowercase letters");
+  var hasUppercase = confirm("Click OK for uppercase letters");
+  var hasNumber = confirm("Click Ok for numbers");
+  var hasSpecial = confirm("Click ok for special characters");
+
+  if(
+    hasSpecial === false &&
+    hasNumber === false &&
+    hasLowercase === false &&
+    hasUppercase === false) {
+
+      alert("Must have at least one option");
+      return;
+    }
+
+  // Variable to store password as it's being concatenated
+  var result = [];
+
+  // Array to store types of characters to include in password
+  var possibleCharacters = [];
+
+  // Array to contain one of each type of chosen character to ensure each will be used
+  var guaranteedCharacters = [];
+
+    // Conditional statement that adds array of special characters into array of possible characters based on user input
+  // Push new random special character to guaranteedCharacters
+  if (options.hasSpecial) {
+    possibleCharacters = possibleCharacters.concat(specialCharacters);
+    guaranteedCharacters.push(getRandom(specialCharacters));
+  }
+}
+
 
       function random(len) {
       var l  = len; 
