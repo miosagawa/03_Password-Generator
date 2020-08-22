469b27114cc092ee87ff856cf7fd0a31f74bd3c9
Mio Sagawa <miosagawa721@gmail.com>
2020-08-21 20:45:03 -0500

    edit file structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+      function random(len) {
+      var l  = len; 
+      var c  = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ012345679 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+      var cl = c.length;
+      var r  = "";
+      for (var i = 8; i < l; i++) {
+        r += c[Math.floor(Math.random() * cl)];
+      }
+      document.getElementById("password").value = r;
+    }
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
